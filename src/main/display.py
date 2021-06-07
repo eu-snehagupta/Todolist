12d7ee234c8299f5bb204bf32616c03bc333ec7f
Sigma <sigma@Snehas-MacBook-Pro.local>
2021-06-07 10:56:32 +0200

    Add working code for display

--- src/main/display.py
+++ src/main/display.py
@@ -1,34 +1,16 @@
-import todolist
-import task
+#
+# def get_task_details():
+#     task_title = input("Enter the task title: ")
+#     task_due_date = input("Enter the task due date: ")
+#     task_status = input("Enter the task status (1) Done, (2) Undone: ")
+#     task_project = input("Enter the task project: ")
+#     return task_title, task_due_date, task_status, task_project
 
-
-def get_task_details():
-    task_title = input("Enter the task title: ")
-    task_due_date = input("Enter the task due date: ")
-    task_status = input("Enter the task status (1) Done, (2) Undone: ")
-    task_project = input("Enter the task project: ")
-    return task_title, task_due_date, task_status, task_project
-
-
-def switch(choice):
-    to_do_list = todolist.Todolist
-    if choice == 1:
-        print(to_do_list.get_list())
-        handle_option()
-    elif choice == 2:
-        a,b,c,d = get_task_details()
-        new_task = task.Task(a,b,c,d)
-        print(new_task)
-        to_do_list.add_to_list(new_task)
-        handle_option()
-    elif choice == 3:
-        print()
-        handle_option()
-    elif choice == 4:
-        print()
-        return
-    else:
-        print("You have entered a wrong option. Try again!")
+def display():
+    display_welcome_message()
+    display_option_details()
+    input_option = int(input())
+    handle_option(input_option)
 
 
 def display_welcome_message():
@@ -51,8 +33,22 @@ def handle_option(option):
         print("Could not convert option to an integer.")
 
 
+def switch(choice):
+    if choice == 1:
+        print("choice1")
+        return display()
+    elif choice == 2:
+        print("choice2")
+        return display()
+    elif choice == 3:
+        print("choice3")
+        return display()
+    elif choice == 4:
+        print("choice4")
+        return
+    else:
+        print("You have entered a wrong option. Try again!")
+
+
 if __name__ == '__main__':
-    display_welcome_message()
-    display_option_details()
-    input_option = input()
-    handle_option(input_option)
+    display()
